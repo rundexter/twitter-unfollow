f849667304292bdd70682080ae9b2129196dccba
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-26 12:55:34 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
+var Twit = require('twit');
+var _ = require('lodash');
+
+var mapAuthOptionsEnv = {
+    'twitter_consumer_key': 'consumer_key',
+    'twitter_consumer_secret': 'consumer_secret',
+    'twitter_access_token': 'access_token',
+    'twitter_access_token_secret': 'access_token_secret'
+};
+
+var pickResult = ['screen_name', 'follow_request_sent', 'following'];
+
 module.exports = {
+    unfollow: function (authOptions, params, callback) {
+        var twitter = new Twit(authOptions);
+
+        // Follow befriended
+        twitter.post('friendships/destroy', params, callback);
+    },
+
     /**
-     * The main entry point for the Dexter module
+     * Get Auth options from Environment.
+     *
+     * @param dexter
+     * @returns {{}}
+     */
+    authOptions: function (dexter) {
+        // twitter auth property
+        var authOptions = {};
+
+        _.map(mapAuthOptionsEnv, function (authOpt, twitterOpt) {
+            if(dexter.environment(twitterOpt)) {
+              // get auth property
+                authOptions[authOpt] = dexter.environment(twitterOpt);
+            } else {
+                // catch no-arguments message
+                this.fail('A ' + twitterOpt + ' environment variable is required for this module');
+            }
+        }, this);
+
+        return authOptions;
+    },
+
+    /**
+     * Allows the authenticating users to follow the user specified in the ID parameter.
      *
      * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.unfollow(this.authOptions(dexter), step.inputs(), function (error, unfollowInfo) {
+            if (error) {
+                // if error - send message
+                this.fail(error);
+            }
+            // return befriendedInfo
+            this.complete(_.pick(unfollowInfo, pickResult));
+        }.bind(this));
     }
 };
